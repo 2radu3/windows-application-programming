3dc67870cfc362380876e72fed4332dc10c9cbb6
2radu3 <2radupmihai3@gmail.com>
2026-03-11 14:29:03 +0200

    Add project files.

---
+++ seminar_04/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace seminar_04
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.tbNume = new System.Windows.Forms.TextBox();
+            this.tbFacultate = new System.Windows.Forms.TextBox();
+            this.tbNote = new System.Windows.Forms.TextBox();
+            this.btnAdauga = new System.Windows.Forms.Button();
+            this.tbStud = new System.Windows.Forms.TextBox();
+            this.btnAfiseazaStud = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tbNume
+            // 
+            this.tbNume.Location = new System.Drawing.Point(13, 13);
+            this.tbNume.Name = "tbNume";
+            this.tbNume.Size = new System.Drawing.Size(100, 20);
+            this.tbNume.TabIndex = 0;
+            // 
+            // tbFacultate
+            // 
+            this.tbFacultate.Location = new System.Drawing.Point(13, 39);
+            this.tbFacultate.Name = "tbFacultate";
+            this.tbFacultate.Size = new System.Drawing.Size(100, 20);
+            this.tbFacultate.TabIndex = 1;
+            // 
+            // tbNote
+            // 
+            this.tbNote.Location = new System.Drawing.Point(13, 65);
+            this.tbNote.Name = "tbNote";
+            this.tbNote.Size = new System.Drawing.Size(100, 20);
+            this.tbNote.TabIndex = 2;
+            // 
+            // btnAdauga
+            // 
+            this.btnAdauga.Location = new System.Drawing.Point(13, 92);
+            this.btnAdauga.Name = "btnAdauga";
+            this.btnAdauga.Size = new System.Drawing.Size(100, 26);
+            this.btnAdauga.TabIndex = 3;
+            this.btnAdauga.Text = "Adauga";
+            this.btnAdauga.UseVisualStyleBackColor = true;
+            this.btnAdauga.Click += new System.EventHandler(this.btnAdauga_Click);
+            // 
+            // tbStud
+            // 
+            this.tbStud.Location = new System.Drawing.Point(209, 45);
+            this.tbStud.Multiline = true;
+            this.tbStud.Name = "tbStud";
+            this.tbStud.Size = new System.Drawing.Size(297, 73);
+            this.tbStud.TabIndex = 4;
+            // 
+            // btnAfiseazaStud
+            // 
+            this.btnAfiseazaStud.Location = new System.Drawing.Point(309, 8);
+            this.btnAfiseazaStud.Name = "btnAfiseazaStud";
+            this.btnAfiseazaStud.Size = new System.Drawing.Size(102, 29);
+            this.btnAfiseazaStud.TabIndex = 5;
+            this.btnAfiseazaStud.Text = "Afiseaza";
+            this.btnAfiseazaStud.UseVisualStyleBackColor = true;
+            this.btnAfiseazaStud.Click += new System.EventHandler(this.btnAfiseazaStud_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnAfiseazaStud);
+            this.Controls.Add(this.tbStud);
+            this.Controls.Add(this.btnAdauga);
+            this.Controls.Add(this.tbNote);
+            this.Controls.Add(this.tbFacultate);
+            this.Controls.Add(this.tbNume);
+            this.Name = "Form1";
+            this.Text = "PrimaFereastra";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbNume;
+        private System.Windows.Forms.TextBox tbFacultate;
+        private System.Windows.Forms.TextBox tbNote;
+        private System.Windows.Forms.Button btnAdauga;
+        private System.Windows.Forms.TextBox tbStud;
+        private System.Windows.Forms.Button btnAfiseazaStud;
+    }
+}
+
---
+++ seminar_04/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace seminar_04
+{
+    public partial class Form1 : Form
+    {
+
+        private List<Student> studenti = new List<Student>();
+
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+            //Form1 form = (Form1)sender -> form.Text;
+            MessageBox.Show($"Se deschide {this.Text}..."); //se afiseaza proprietatea de Text din proprierties
+        }
+
+        private void btnAdauga_Click(object sender, EventArgs e) {
+            string nume = tbNume.Text;
+            string facultate = tbFacultate.Text;
+            string[] noteString = tbNote.Text.Split(',');
+            int[] noteInt = new int[noteString.Length];
+            for(int i = 0; i < noteString.Length; i++) {
+                noteInt[i] = int.Parse(noteString[i]);
+            }
+            Student s = new Student(nume, facultate, noteInt);
+            s.CalculeazaMedie();
+            MessageBox.Show(s.ToString());
+            studenti.Add(s);
+        }
+
+        private void btnAfiseazaStud_Click(object sender, EventArgs e) {
+            foreach(Student s in studenti) {
+                tbStud.Text += s.ToString() + Environment.NewLine;
+            }
+        }
+    }
+}
